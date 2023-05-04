136de9334f4817f5773681f42bda9cffddb77c23
Coding-Jarl <senvisage.spam@gmail.com>
2023-05-04 18:07:46 +0200

    refactor(cleanup): remove useless components

--- src/components/aside.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-import styles from './aside.module.scss'
-
-interface Props {
-  children: ReactNode
-}
-export default function Aside({ children }: Props) {
-  return <aside className={styles.aside}>{children}</aside>
-}
--- src/components/main.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-import styles from './main.module.scss'
-
-interface Props {
-  children: ReactNode
-}
-export default function Main({ children }: Props) {
-  return <main className={styles.main}>{children}</main>
-}
