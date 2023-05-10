5d27265156b3dfc8e656ae0c4a508a661cb93cdd
Coding-Jarl <senvisage.spam@gmail.com>
2023-05-11 00:33:00 +0200

    feat: add support for deck save

--- src/components/deck.tsx
+++ src/components/deck.tsx
@@ -2,32 +2,47 @@ import styles from './deck.module.scss'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '@/lib/redux/store'
 import { removeCard } from '@/lib/redux/slices/deck'
+import axios from 'axios'
 
 export default function Deck() {
   const cards = useSelector((state: RootState) => state.deck.cards)
   const dispatch = useDispatch()
 
+  const hSave = async () => {
+    const myData = {
+      description: 'totoland',
+      name: 'test-' + Math.random() * 100,
+      idAuthor: 1,
+      cards: cards,
+    }
+
+    await axios.post('http://localhost:3000/api/decks', myData)
+  }
+
   return (
-    <ul className={styles.deck}>
-      {cards.map((card) => (
-        <li key={card.id}>
-          <button
-            onClick={() =>
-              dispatch(
-                removeCard({
-                  id: card.id,
-                  uuid: card.id,
-                  count: 1,
-                  name: '',
-                })
-              )
-            }
-          >
-            -
-          </button>
-          {card.count}x {card.name}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className={styles.deck}>
+        {cards.map((card) => (
+          <li key={card.id}>
+            <button
+              onClick={() =>
+                dispatch(
+                  removeCard({
+                    id: card.id,
+                    uuid: card.id,
+                    count: 1,
+                    name: '',
+                  })
+                )
+              }
+            >
+              -
+            </button>
+            {card.count}x {card.name}
+          </li>
+        ))}
+      </ul>
+      <button onClick={hSave}>Save</button>
+    </>
   )
 }
--- src/pages/api/decks.ts
+++ src/pages/api/decks.ts
@@ -1,21 +1,60 @@
 import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export async function listDecks() {
+export async function listDecks({
+  name = '',
+  description = '',
+  idAuthor = '',
+}: Partial<{
+  name: string
+  description: string
+  idAuthor: string
+}>) {
   const prisma = new PrismaClient()
-  const decks = prisma.deck.findMany()
+  const decks = prisma.deck.findMany({
+    where: {
+      OR: {
+        name: { contains: name },
+        description: { contains: description },
+        idAuthor: Number(idAuthor) || undefined,
+      },
+    },
+    include: {
+      contents: {
+        select: {
+          card: true,
+          count: true,
+        },
+      },
+    },
+  })
   return decks
 }
 export async function createDeck({
   name,
   description,
   idAuthor,
+  cards,
 }: {
   name: string
   description: string
   idAuthor: number
+  cards: Array<{
+    id: string
+    uuid: string
+    name: string
+    count: number
+  }>
 }) {
   const prisma = new PrismaClient()
+
+  await prisma.card.createMany({
+    data: cards.map((card) => ({ name: card.name, uuid: card.uuid })),
+    skipDuplicates: true,
+  })
+
+  const allCards = await prisma.card.findMany()
+
   const newDeck = prisma.deck.create({
     data: {
       description: description,
@@ -23,6 +62,16 @@ export async function createDeck({
       author: {
         connect: {
           id: idAuthor,
+        },
+      },
+      contents: {
+        createMany: {
+          data: cards.map((card) => ({
+            count: card.count,
+            idCard: allCards.find((candidate) => candidate.uuid === card.uuid)
+              ?.id as number,
+          })),
+          skipDuplicates: true,
         },
       },
     },
@@ -38,7 +87,12 @@ export default async function handler(
 ) {
   switch (req.method) {
     case 'GET':
-      const decks = await listDecks()
+      const fields: Partial<{
+        name: string
+        description: string
+        idAuthor: string
+      }> = req.query
+      const decks = await listDecks(fields)
       return res.status(200).json(decks)
     case 'POST':
       const rawData = req.body
