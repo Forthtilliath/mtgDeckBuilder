5ada2da0b78bea81a9bcad9461ca7bbc9afd8feb
Forth <33458877+Forthtilliath@users.noreply.github.com>
2023-05-17 03:21:15 +0200

    fix: vercel

--- src/components/errorBoundary.tsx
+++ src/components/errorBoundary.tsx
@@ -9,7 +9,7 @@ class ErrorBoundary extends Component<PropsWithChildren, State> {
     hasError: false,
   }
 
-  public static getDerivedStateFromError(_: Error): State {
+  public static getDerivedStateFromError(_err: Error): State {
     // Update state so the next render will show the fallback UI.
     return { hasError: true }
   }
